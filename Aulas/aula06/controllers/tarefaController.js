17136c8ed6620012c55c10015b075a4df3aab6a5
unknown <2224290017@ACADEMICO.IESB.BR>
2025-09-23 21:33:51 -0300

    Aula 06

--- Aulas/aula06/controllers/tarefaController.js
+++ Aulas/aula06/controllers/tarefaController.js
@@ -1,13 +1,37 @@
-const tarefas = [];
+const model = require("../models/tarefaModel");
 
 const listarTarefas = (req, res) => {
-    res.json(tarefas);
+    res.json(model.listar());
 };
 
 const criarTarefa = (req, res) =>{
-    const novaTarefa = {...req.body, id:tarefas.length + 1};
-    tarefas.push(novaTarefa);
+    const novaTarefa = model.criar(req.body);
     res.status(201).json(novaTarefa);
 };
 
-module.exports = { listarTarefas, criarTarefa };
+const buscarTarefa = (req, res, next) =>{
+    const { id } =  req.params;
+    const tarefaEncontrada = model.obter(id);
+    if (tarefaEncontrada) return next();
+    res.status(404).json({msg: "Tarefa não encontrada"});
+}
+
+const obterTarefa = (req, res) => {
+    const { id } =  req.params;
+    const tarefaEncontrada = model.obter(id);
+    res.json(tarefaEncontrada);
+};
+
+const atualizarTarefa = (req, res) =>{
+    const { id } = req.params;
+    const tarefaEncontrada = model.atualizar({id, ...req.body});
+    res.json(tarefaEncontrada);
+ };
+
+const deletarTarefa = (req, res) =>{
+    const { id } = req.params;
+    model.deletar(id);
+    res.status(204).end();
+};
+ 
+module.exports = { buscarTarefa, listarTarefas, criarTarefa, obterTarefa, atualizarTarefa, deletarTarefa };
---
+++ Aulas/aula06/models/tarefaModel.js
@@ -0,0 +1,37 @@
+const tarefas = [];
+
+const listar = () => {
+return tarefas;
+
+};
+
+const criar = (dados) => {
+    const novaTarefa = {...dados, id:tarefas.length + 1};
+    tarefas.push(novaTarefa);
+    return novaTarefa;
+};
+
+const obter = (id) =>{
+    const tarefaEncontrada = tarefas.find(item  => item.id === parseInt(id));
+    return tarefaEncontrada;
+
+};
+
+const atualizar = (tarefa) =>{
+    const tarefaEncontrada = tarefas.find(item => item.id == tarefa.id);
+    if(tarefaEncontrada) {
+    tarefaEncontrada.name = tarefa.name;
+    tarefaEncontrada.concluida = tarefa.concluida;
+}
+return tarefaEncontrada;
+};
+
+const deletar = (id) => {
+    const posicao = tarefas.findIndex((item) => item.id == id);
+    if (posicao >= 0){
+        tarefas.splice(posicao, 1);
+    }
+    return posicao;
+};
+
+module.exports = {listar, criar, obter, atualizar, deletar}
