05ed5f0de813711bfec00116cc28add89f6668d1
unknown <2224290017@ACADEMICO.IESB.BR>
2025-10-21 21:43:32 -0300

    Adicionando códigos aula 9

--- Aulas/aula09/aula09/tests/tarefaRouter.test.js
+++ Aulas/aula09/aula09/tests/tarefaRouter.test.js
@@ -8,8 +8,17 @@ describe('Testes do recurso /tarefas', () => {
     test('POST / deve retornar 201', async () =>{
         const response = await request.post(url).send({ nome: "Estudar" });
         expect(response.status).toBe(201);
+        expect(response.body._id).toBeDefined(); 
+        expect(response.body.nome).toBe("Estudar");
+        expect(response.body.concluida).toBe(false);
         id = response.body._id;
-    });
+        });
+    test('POST / deve retornar 422', async () =>{
+        const response = await request.post(url);
+        expect(response.status).toBe(422);
+        expect(response.body.msg).toBe("Nome da tarefa é obrigatorio");
+    });    
+
 
     test('GET / deve retornar 200', async () =>{
         const response = await request.get(url);
@@ -19,15 +28,49 @@ describe('Testes do recurso /tarefas', () => {
     test('GET / deve retornar 200', async () =>{
         const response = await request.get(`${url}/${id}`);
         expect(response.status).toBe(200);
+        expect(response.body._id).toBeDefined(); 
+        expect(response.body.nome).toBe("Estudar");
+        expect(response.body.concluida).toBe(false);
+    });
+    test('GET / deve retornar 404', async () =>{
+        const response = await request.get(`${url}/000000000000000000000000`);
+        expect(response.status).toBe(404);
+        expect(response.body.msg).toBe("Tarefa não encontrada");
+    });
+    test('GET / deve retornar 400', async () =>{
+        const response = await request.get(`${url}/0`);
+        expect(response.status).toBe(400);
+        expect(response.body.msg).toBe("ID inválido");
     });
     test('PUT / deve retornar 200', async () =>{
         const response = await request.put(`${url}/${id}`).send({
             nome: "Estudar Express", concluida: true
         });
         expect(response.status).toBe(200);
+        expect(response.body.nome).toBe("Estudar Express");
+        expect(response.body.concluida).toBe(true);
+    });
+    test('PUT / deve retornar 404', async () =>{
+        const response = await request.put(`${url}/000000000000000000000000`);
+        expect(response.status).toBe(404);
+        expect(response.body.msg).toBe("Tarefa não encontrada");
+    });
+    test('PUT / deve retornar 400', async () =>{
+        const response = await request.put(`${url}/0`);
+        expect(response.status).toBe(400);
+        expect(response.body.msg).toBe("ID inválido");
     });
     test('DELETE / deve retornar 204', async () =>{
     const response = await request.delete(`${url}/${id}`);
         expect(response.status).toBe(204);
+    });
+    test('DELETE / deve retornar 400', async () =>{
+        const response = await request.delete(`${url}/0`);
+        expect(response.status).toBe(400);
+        expect(response.body.msg).toBe("ID inválido");
+    });
+    test('DELETE / deve retornar 404', async () =>{
+        const response = await request.delete(`${url}/${id}`);
+        expect(response.status).toBe(404);
     });
 });
